6f94c17ec683d65d0fc135e247fb516ca8859d00
Leo Cucinell <leocucinell@gmail.com>
2021-07-30 11:37:25 -0700

    addedthe session configuration to the server file

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 /* SECTION: External modules */
 const express = require("express");
+const session = require("express-sessions");
+const MongoStore = require("connect-mongo")
 
 /* SECTION: Internal modules */
 
@@ -8,6 +10,19 @@ const app = express();
 
 /* SECTION: Configure App  */
 const PORT = 4000;
+
+//we can now view the session with req.session
+app.use(
+    session({
+      store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
+      secret: process.env.SECRET,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7 * 2, // two weeks
+      },
+    })
+);
 
 /* SECTION: Middleware */
 
